5521643f19b92869d4243739ae1805cd068f42e0
小明同学i <923727421@qq.com>
2021-11-01 17:58:24 +0800

    ~

---
+++ study/vt.go
@@ -0,0 +1,15 @@
+/*
+ * @Description:
+ * @Autor: 小明～
+ * @Date: 2021-11-01 09:06:59
+ * @LastEditors: 小明～
+ * @LastEditTime: 2021-11-01 09:07:00
+ */
+package main
+
+import "fmt"
+
+func main() {
+	adf := []byte{}
+	fmt.Printf("%T===%v", adf, adf)
+}
